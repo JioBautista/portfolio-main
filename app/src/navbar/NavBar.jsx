3fb062f652324cc5377cdf2d63fa0b5788277a66
Jio Bautista <98562544+JioBautista@users.noreply.github.com>
2023-11-20 16:33:29 -0500

    Update NavBar.jsx

--- app/src/navbar/NavBar.jsx
+++ app/src/navbar/NavBar.jsx
@@ -17,7 +17,7 @@ function NavBar() {
         </Link>
 
         <Link to="/about" className={styles.navlinks}>
-          About
+          Hello
         </Link>
 
         <Link to="/project" className={styles.navlinks}>
