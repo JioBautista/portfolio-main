88a386af2c25cdb7cb7adfb0f2d59321895d6655
Jio Bautista <98562544+JioBautista@users.noreply.github.com>
2024-04-23 13:36:22 -0400

    Update About.jsx

--- app/src/pages/About.jsx
+++ app/src/pages/About.jsx
@@ -16,10 +16,20 @@ function About() {
         textAlign={"center"}
         marginBottom={3}
       >
-        I'm a passionate developer who taught myself the ins and outs of both
-        front-end and back-end development. I'm constantly learning and pushing
-        myself to grow. I'm excited to explore new challenges, collaborate with
-        others, and contribute my skills to innovative projects.
+        I'm Jio, a passionate programmer with a knack for problem-solving and a
+        drive to build beautiful, functional applications. For the past 4 years,
+        I thrived in the fast-paced environment of retail management, honing my
+        communication, teamwork, and leadership skills. Recently, my desire to
+        create impactful experiences led me down the path of self-taught
+        programming. Within the past year, I've immersed myself in the world of
+        web development, mastering the fundamentals of HTML, CSS, and
+        JavaScript. Eager to push my boundaries, I delved into frameworks like
+        React and explored back-end development with Python and Django. My
+        passion lies in building user interfaces that are not only visually
+        appealing but also intuitive and user-friendly. Though my programming
+        journey is young, I'm a quick learner fueled by a relentless curiosity.
+        I'm eager to leverage my diverse skillset and collaborative spirit to
+        contribute to a dynamic development team.
       </Typography>
       <Box sx={{ p: 2 }}>
         <Stack
