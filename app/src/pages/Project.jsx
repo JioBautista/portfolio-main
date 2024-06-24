d1744e311721ac9746014702177e0cb65149024b
Jio Bautista <98562544+JioBautista@users.noreply.github.com>
2024-06-24 17:23:00 -0400

    Update Project.jsx

--- app/src/pages/Project.jsx
+++ app/src/pages/Project.jsx
@@ -51,7 +51,7 @@ function Project() {
         <Card sx={{ maxWidth: 375 }}>
           <CardHeader title="Invoice App" subheader="React & Django" />
           <CardContent>
-            <Typography>
+            <Typography sx={{ mb: 1 }}>
               This invoice management app allows you to create, view, edit, and
               delete invoices (CRUD operations) with ease. Built with a
               user-friendly React frontend and a powerful Django backend using
@@ -65,7 +65,7 @@ function Project() {
               Link to Github
             </Link>
           </CardContent>
-          <CardActions disableSpacing>
+          {/* <CardActions disableSpacing>
             <ExpandMore expand={expanded} onClick={handleExpandClick}>
               <ExpandMoreIcon />
             </ExpandMore>
@@ -90,27 +90,35 @@ function Project() {
               <Typography>- Vercel</Typography>
               <Typography>- DigitalOcean</Typography>
             </CardContent>
-          </Collapse>
+          </Collapse> */}
         </Card>
 
         <Card sx={{ maxWidth: 375 }}>
-          <CardHeader title="Invoice App" subheader="React & Django" />
+          <CardHeader title="E-Commerce Website" subheader="React & Tailwind" />
           <CardContent>
-            <Typography>
-              This invoice management app allows you to create, view, edit, and
-              delete invoices (CRUD operations) with ease. Built with a
-              user-friendly React frontend and a powerful Django backend using
-              REST framework.
+            <Typography sx={{ mb: 1 }}>
+              This project showcases a complete e-commerce website built
+              entirely on React & Tailwind CSS. It achieves cart and checkout
+              functionality without a traditional backend.
             </Typography>
             <Link
-              href="https://github.com/JioBautista/invoice-app"
+              href="https://github.com/JioBautista/audiophile-ecommerce-website"
               variant="body1"
               underline="hover"
+              sx={{ display: "block", mb: 1 }}
             >
               Link to Github
             </Link>
+            <Link
+              href="https://audiophile-ecommerce-jio.vercel.app/"
+              variant="body1"
+              underline="hover"
+              display={"block"}
+            >
+              Link to Project
+            </Link>
           </CardContent>
-          <CardActions disableSpacing>
+          {/* <CardActions disableSpacing>
             <ExpandMore expand={expanded} onClick={handleExpandClick}>
               <ExpandMoreIcon />
             </ExpandMore>
@@ -121,21 +129,12 @@ function Project() {
               <Typography>- React</Typography>
               <Typography>- React-Router V6</Typography>
               <Typography>- React-Hook-Form</Typography>
-              <Typography>- Axios</Typography>
-              <Typography>- Material UI</Typography>
               <Typography>- Zustand State Management</Typography>
-
-              <Typography variant="h6">Backend:</Typography>
-              <Typography>- Django & REST framework</Typography>
-              <Typography>- Django-cors-headers</Typography>
-              <Typography>- Gunicorn</Typography>
-              <Typography>- Psycopg2</Typography>
 
               <Typography variant="h6">Hosting:</Typography>
               <Typography>- Vercel</Typography>
-              <Typography>- DigitalOcean</Typography>
             </CardContent>
-          </Collapse>
+          </Collapse> */}
         </Card>
       </Stack>
     </Container>
